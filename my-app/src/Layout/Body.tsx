43d1eac77a3387b1d7ac9fe1a36f10f444f31eb4
eric-k-chu <kiyuen88ec@gmail.com>
2023-10-14 21:46:21 -0700

    Added reset button and WIP copy list button

--- my-app/src/Layout/Body.tsx
+++ my-app/src/Layout/Body.tsx
@@ -1,7 +1,7 @@
 import { Button, List } from '../Components/index'
 import { useState } from 'react';
 
-const classList = [
+let classList = [
   'Kaitlin',
   'Ross',
   'Tyler',
@@ -17,18 +17,33 @@ const classList = [
   'Daniel'
 ];
 
+const copyList = classList.map(n => n);
+
+const completedList: string[] = [];
+
 function Body() {
   const [list, setList] = useState<string[]>([]);
+  const [isComplete, setComplete] = useState(false);
 
   function handleRandomAdd(): void {
     if (classList.length) {
       const randIndex = Math.floor(Math.random() * classList.length);
+      completedList.push(classList[randIndex]);
       const newList = [...list, classList[randIndex]];
       classList.splice(randIndex, 1);
       setList(newList);
     } else {
-      alert('Empty');
+      setComplete(true);
     }
+  }
+
+  function handleCopyList(): void {
+    alert('yes');
+  }
+
+  function handleReset(): void {
+    classList = copyList.map(n => n);
+    setList([]);
   }
 
   return (
@@ -39,7 +54,13 @@ function Body() {
       </div>
 
       <div className='flex justify-around items-center'>
+        { isComplete &&
+          <Button text="Copy List" onCustomClick={handleCopyList} styles='rounded-3xl w-40 bg-dark-gray-1'/>
+        }
         <Button text="Get Random" onCustomClick={handleRandomAdd} styles='rounded-3xl w-40 bg-dark-gray-1'/>
+        { isComplete &&
+          <Button text="Reset" onCustomClick={handleReset} styles='rounded-3xl w-40 bg-dark-gray-1'/>
+        }
       </div>
     </div>
   )
