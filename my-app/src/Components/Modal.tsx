06349dc7d8a9859caee7cd3204f7cd2ea3769d94
eric-k-chu <kiyuen88ec@gmail.com>
2023-10-17 19:48:45 -0700

    Changed handleSubmit to use event object

--- my-app/src/Components/Modal.tsx
+++ my-app/src/Components/Modal.tsx
@@ -6,15 +6,17 @@ function InsertForm({ isActive, onInsert, onCustomClick }: {isActive: Function, 
   
     function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
       e.preventDefault();
-      const textArea = document.querySelector('form')![0] as HTMLTextAreaElement;
-      onInsert(textArea.value.split('\n'));
+      const formData = e.target as HTMLFormElement;
+      const textArea = formData[0] as HTMLTextAreaElement;
+      const val = textArea.value;
+      onInsert(val.split('\n'));
       isActive(false);
     }
   
     return (
       <div className='flex flex-col items-center justify-center absolute top-0 left-0 w-full h-full z-50'>
         <div className='w-72 h-[32rem] bg-dark-gray-1 p-2 rounded-md shadow-lg'>
-          <form method='post' onSubmit={handleSubmit} className='flex flex-col items-center justify-center h-[31rem] gap-y-4'>
+          <form method='post' onSubmit={e => handleSubmit(e)} className='flex flex-col items-center justify-center h-[31rem] gap-y-4'>
             <label htmlFor={textAreaID}>Type the names in your class.</label>
             <textarea id={textAreaID} rows={15} className='resize-none bg-dark-gray-2'></textarea>
             <div className='flex justify-between text-xs w-64 px-3'>
