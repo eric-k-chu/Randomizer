9b705317960c9daae93c641f2ffed4c79f8a2044
eric-k-chu <kiyuen88ec@gmail.com>
2023-10-20 12:39:10 -0700

    added list appear on search

--- my-app/src/Components/SearchBar.tsx
+++ my-app/src/Components/SearchBar.tsx
@@ -9,20 +9,25 @@ function SearchBar({ placeText }: SearchBarProps) {
   const [showList, setShowList] = useState(false);
 
   return (
-    <div className='rounded-2xl bg-[#F5F5F5] w-full h-full flex justify-center items-center'>
+    <div className='rounded-2xl focus-within:rounded-b-none bg-white w-full h-full flex justify-center items-center relative'>
       <img src={searchIcon} />
       <input 
         type='text' 
         placeholder={placeText} 
-        className='text-black bg-transparent px-2'/>
-      <div className='w-full z-index-50 absolute flex justify-center'>
-        <div className='bg-black'>
-          <p className='p-2'>HEllo</p>
-          <p className='p-2'>HEllo</p>
-          <p className='p-2'>HEllo</p>
-          <p className='p-2'>HEllo</p>
+        className='text-black bg-transparent px-2' 
+        onFocus={() => setShowList(true)}
+        onBlur={() => setShowList(false)}/>
+
+      {showList && 
+        <div className='w-full z-index-50 absolute flex top-8 justify-center rounded-b-2xl shadow-lg'>
+          <div className='w-full bg-black text-black rounded-b-2xl'>
+            <p className='p-2'>HEllo</p>
+            <p className='p-2'>HEllo</p>
+            <p className='p-2'>HEllo</p>
+            <p className='p-2'>HEllo</p>
+          </div>
         </div>
-      </div>
+      }
     </div>
   );
 }
