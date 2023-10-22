7de3aa799b2332450750d88423d3534b366dc75a
eric-k-chu <kiyuen88ec@gmail.com>
2023-10-22 15:32:28 -0700

    WIP choose list

--- my-app/src/Components/List.tsx
+++ my-app/src/Components/List.tsx
@@ -1,3 +1,5 @@
+import data from '../data'
+
 type ListProps = {
   customList: string[];
   styles?: string;
@@ -12,8 +14,15 @@ function List({ customList, styles }: ListProps) {
 
 function SearchList({ customList, styles, maxSize }: ListProps) {
   const limit = Number(maxSize);
+
+  function handleOnListItemClick(): void {
+    // TODO
+  }
+
   return (
-    <ul className={styles}>{customList.map((n, i) => <li key={i} className="px-2 hover:bg-slate-300 py-2 w-full">{n}</li>).slice(0, limit)}</ul>
+    <ul className={styles}>
+      {customList.map((n, i) => <li key={i} className="px-2 hover:bg-slate-300 py-2 w-full">{n}</li>).slice(0, limit)}
+    </ul>
   )
 }
 
